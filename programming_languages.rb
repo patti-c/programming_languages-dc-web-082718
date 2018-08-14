29a5f4fff815a5828726918c54ca1ce2ba156a0b
Patricia Carter <patricia4194@gmail.com>
2018-08-14 01:00:58 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -6,12 +6,10 @@ def reformat_languages(languages)
   
   languages.each do |style, language_hash|
     language_hash.each do |language_key, type|
-      # new_hash[language_key] = {
-      #   :style => [style],
-      #   :type => languages[style][language_key][:type]}
-      
-      new_hash[language_key] << {:style => [style]}}
-      new_hash[language_key] << {:type => languages[style][language_key][type]}
+      if 
+        new_hash[language_key] = {
+          :style => [style],
+          :type => languages[style][language_key][:type]}
       
     end
   end 
