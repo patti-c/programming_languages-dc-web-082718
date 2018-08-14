ee351e62bf0d4048577cc59fe46cf2a7b22cdb23
Patricia Carter <patricia4194@gmail.com>
2018-08-14 00:28:59 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,10 +1,12 @@
 def reformat_languages(languages)
   # Iterate over all of the languages to make them the top level of the hash,
-  # while placing in :oo or :functional, then 
+  # while placing in :oo or :functional
   
-  # 
+  new_hash = {}
   
-  # 
+  languages.each do |style, language|
+    new_hash[language] = {
+      :style => [], 
   
   
 end
